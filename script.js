c17cb3647d6b2c9095131b2da3445f9b22310447
Gunnar Bjorkman <gunnarsbjorkman@gmail.com>
2017-06-18 23:39:19 -0400

    button input locations were fixed
    no longer will numbers from buttons be located on the end of the string, instead the string is cut where the cursor is and patched back up with the button parameter in the middle

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+window.onload = function(){
+  document.getElementById('results').focus();
+}
+
 document.addEventListener("keydown", function (e) {
     if (e.keyCode === 13) {  //checks whether the pressed key is "Enter"
         execute();
@@ -17,22 +21,46 @@ function execute(){
 
   console.log(input);
 
+  setTimeout(function () {
+    document.getElementById("results").style.width = ((document.getElementById("results").value.length) * 36) + 'px';
+  }, 1);
+
   document.getElementById("results").value = math.eval(input);
 
 }
 
-function numberInput(x){
+function buttonInput(x){
+
+  if(isNaN(x)){
+
+  } else {
+    symbolInput();
+  }
 
   console.log(x);
 
+  var caret_start = document.getElementById("results").selectionStart
+  var caret_end = document.getElementById("results").selectionEnd
+
   var current = document.getElementById("results").value;
 
+  var result_start = current.slice(0, caret_start);
+  var result_end = current.slice(caret_end, current.length) ;
+
+  console.log(result_start);
+  console.log(result_end);
+  console.log(current.length);
+
   console.log(((document.getElementById("results").value.length + 1) * 36) + 'px');
-
   document.getElementById("results").style.width = ((document.getElementById("results").value.length + 1) * 36) + 'px';
+  document.getElementById("results").value = result_start + x + result_end;
+}
 
-  document.getElementById("results").value = current + x;
-
-
-
+function symbolInput(){
+  var caret_start = document.getElementById("results").selectionStart;
+  var caret_end = document.getElementById("results").selectionEnd;
+  console.log(caret_start);
+  setTimeout(function () {
+    document.getElementById('results').focus();document.getElementById('results').setSelectionRange(caret_start + 1,caret_end + 1);
+  }, 1);
 }
